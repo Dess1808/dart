816565f423e677853ee2e1b37cc9351c27bcc86a
Gabriel Souza Martins <gabrielmartins0898@gmail.com>
2022-04-06 14:05:24 -0400

    atualizando mais informacao

---
+++ print.dart
@@ -0,0 +1,12 @@
+// void printerInteger(int aNumber); nao define prototipos
+
+void printerInteger(int aNumber){
+    print('The number is $aNumber');
+}
+
+void main(){
+    //definindo uma variavel sem tipo especifico
+    var number = 42;
+    printerInteger(number);
+}
+
